ca7fe5cd7fc44c190951513ee6c0119e405d6243
Charles maina <muragemaina09@gmail.com>
2019-09-11 11:39:49 +0300

    deploy

---
+++ common/config/main.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\Admin',
+            'enableAutoLogin' => true,
+            'identityCookie' => [
+                'name' => '_backendUser', // unique for backend
+            ]
+        ],
+        'session' => [
+            'name' => 'PHPBACKSESSID',
+            'savePath' => sys_get_temp_dir(),
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '7021135929',
+            'csrfParam' => '_backendCSRF',
+        ],
+    ],
+];
